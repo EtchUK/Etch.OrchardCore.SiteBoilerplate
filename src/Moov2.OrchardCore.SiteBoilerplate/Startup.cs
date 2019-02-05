acf76821fde63166dbc4be472966bd0933c17294
mobinzk <mobinzk@gmail.com>
2019-02-05 16:29:27 +0000

    Add orchard core to the project
    - Orchard core 1.0.0-beta3 added (This required netcoreapp2.2)

--- src/Moov2.OrchardCore.SiteBoilerplate/Startup.cs
+++ src/Moov2.OrchardCore.SiteBoilerplate/Startup.cs
@@ -8,6 +8,7 @@ namespace Moov2.OrchardCore.SiteBoilerplate {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddOrchardCms();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -16,9 +17,7 @@ namespace Moov2.OrchardCore.SiteBoilerplate {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseOrchardCore();
         }
     }
 }
