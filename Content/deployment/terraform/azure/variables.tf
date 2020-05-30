f5e710b6155cdc250be74bfbe713e4e854d6f690
Peter Keating <pete.keating@etchuk.com>
2020-05-30 23:16:17 +0100

    Change root terraform variables to be uppercase

--- Content/deployment/terraform/azure/variables.tf
+++ Content/deployment/terraform/azure/variables.tf
@@ -1,52 +1,52 @@
-variable "env" {
+variable "ENV" {
   description = "Environment descriptor"
 }
 
-variable "hostnames" {
+variable "HOSTNAMES" {
   description = "Hostnames to bind to the app service"
   type        = list(string)
   default     = []
 }
 
-variable "le_client_id" {
+variable "LE_CLIENT_ID" {
   description = "Lets Encrypt APP ID"
 }
 
-variable "le_client_secret" {
+variable "LE_CLIENT_SECRET" {
   description = "Lets Encrypt App ID"
 }
 
-variable "le_subscription_id" {
+variable "LE_SUBSCRIPTION_ID" {
   description = "Lets Encrypt Subscription ID"
 }
 
-variable "le_tenant" {
+variable "LE_TENANT" {
   description = "Lets Encrypt Tenant"
 }
 
-variable "project" {
+variable "PROJECT" {
   description = "Project name for resource names"
-  default     = "Orchard Core Site Boilerplate"
+  default     = "Play CMS Demo"
 }
 
-variable "rg_name" {
+variable "RG_NAME" {
   description = "Name of resource group to create infrastructure in"
 }
 
-variable "rg_name_alt" {
+variable "RG_NAME_ALT" {
   description = "Name of alternative resource group to create infrastructure in, used for items like CDN which can only be created in 'major' regions"
   default     = ""
 }
 
-variable "sp_name" {
+variable "SP_NAME" {
   description = "Name of service plan to create app service in"
 }
 
-variable "sql_elastic_pool" {
+variable "SQL_ELASTIC_POOL" {
   description = "Elastic pool to assign SQL database to"
 }
 
-variable "sql_server_name" {
+variable "SQL_SERVER_NAME" {
   description = "Name of SQL server to create database in"
 }
 
