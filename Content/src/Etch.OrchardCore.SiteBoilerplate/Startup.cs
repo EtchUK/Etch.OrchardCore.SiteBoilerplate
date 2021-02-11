4de7d3b0c849076686c666198806162cc2f85d90
mobinzk <mobinzk@gmail.com>
2021-02-11 11:04:08 +0000

    Fix form value count limit 1024 exceeded
    Increased to 4MB.
    
    Solution:
    https://github.com/OrchardCMS/OrchardCore/issues/3422

--- Content/src/Etch.OrchardCore.SiteBoilerplate/Startup.cs
+++ Content/src/Etch.OrchardCore.SiteBoilerplate/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,6 +12,7 @@ namespace Etch.OrchardCore.SiteBoilerplate
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<FormOptions>(options => options.ValueCountLimit = 4096);
             services.AddOrchardCms();
         }
 
