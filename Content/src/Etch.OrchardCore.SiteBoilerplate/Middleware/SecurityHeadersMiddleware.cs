d37776c4e8d46f8e70c622e8a69c7ec4469b5a84
Peter Keating <pete.keating@etchuk.com>
2023-07-10 15:24:19 +0100

    Change X-Frame-Options to SAMEORIGIN

--- Content/src/Etch.OrchardCore.SiteBoilerplate/Middleware/SecurityHeadersMiddleware.cs
+++ Content/src/Etch.OrchardCore.SiteBoilerplate/Middleware/SecurityHeadersMiddleware.cs
@@ -15,7 +15,7 @@ namespace Etch.OrchardCore.SiteBoilerplate.Middleware
         public Task Invoke(HttpContext httpContext)
         {
             httpContext.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
-            httpContext.Response.Headers["X-Frame-Options"] = "DENY";
+            httpContext.Response.Headers["X-Frame-Options"] = "SAMEORIGIN";
             httpContext.Response.Headers["X-XSS-Protection"] = "1; mode=block";
             httpContext.Response.Headers["X-Content-Type-Options"] = "nosniff";
             return _next.Invoke(httpContext);
